3b1c69463e4446359dc6ea6f50095b3b870f8de1
Armi64bit <bahaeddine.bouzid@esprit.tn>
2023-10-24 14:50:31 +0100

    usersevice start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { SliderComponent } from './slider/slider.component';
 import { AccountsComponent } from './user/components/accounts/accounts.component';
--- src/app/user/components/list-user/list-user.component.ts
+++ src/app/user/components/list-user/list-user.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../../../models/user';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-list-user',
@@ -11,15 +12,17 @@ export class ListUserComponent {
   category: string = '';
   maclasse1 = 'cl1';
   maclasse2 = 'cl2';
-  constructor(private ac: ActivatedRoute) {
+  constructor(private ac: ActivatedRoute,private us:UserService) {
     //injecter le service activatedroute ce composant
     ac.snapshot.paramMap.get('');
     // console.log(this.ac.snapshot.paramMap.get(''));
   }
+  list: User[] =[]
 
   //methode hook qui fait partie de cycle de vie d'un composant
   //apple automatiquement apres la creaction du composant
   ngOnInit() {
+    this.list=this.us.getAllUser();
     // console.log(this.ac.snapshot.params['category']);
 // this.category=this.ac.snapshot.params['category'];
     console.log("init listuser");
@@ -29,63 +32,6 @@ export class ListUserComponent {
   }
   green: string = 'green';
 
-  list: User[] = [
-    {
-      id: 1,
-      firstName: 'Mila',
-      lastName: ' Kunis',
-      birthDate: '1999-06-30',
-      accountCategory: 'Admin',
-      email: 'mila@kunis.com',
-      password: 'test',
-      picture: 'https://bootdey.com/img/Content/avatar/avatar3.png',
-      profession: 'Software Engineer',
-    },
-    {
-      id: 2,
-      firstName: 'George',
-      lastName: 'Clooney',
-      birthDate: '1999-06-30',
-      accountCategory: 'Customer',
-      email: 'marlon@brando.com',
-      password: 'test',
-      picture: 'https://bootdey.com/img/Content/avatar/avatar2.png',
-      profession: 'Software Engineer',
-    },
-    {
-      id: 3,
-      firstName: 'George',
-      lastName: 'Clooney',
-      birthDate: '1999-06-30',
-      accountCategory: 'Customer',
-      email: 'marlon@brando.com',
-      password: 'test',
-      picture: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-      profession: 'Software Engineer',
-    },
-    {
-      id: 4,
-      firstName: 'Ryan',
-      lastName: 'Gossling',
-      birthDate: '1999-06-30',
-      accountCategory: 'Golden',
-      email: 'Ryan@nicholson.com',
-      password: 'test',
-      picture: 'https://bootdey.com/img/Content/avatar/avatar4.png',
-      profession: 'Software Engineer',
-    },
-    {
-      id: 5,
-      firstName: 'Robert',
-      lastName: 'Downey',
-      birthDate: '1999-06-30',
-      accountCategory: 'Blocked Account',
-      email: 'robert@nicholson.com',
-      password: 'test',
-      picture: 'https://bootdey.com/img/Content/avatar/avatar5.png',
-      profession: 'Software Engineer',
-    },
-  ];
 
   delete(pos: number) {
     this.list.splice(pos, 1);
